08946437973b6d199a3f636d8c4c619e2fc40f16
Richard Lucas <richardlucasfm@gmail.com>
2024-08-08 20:42:57 -0300

    thread id on session state

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# MergeMind
+# FreireAI
 
 Projeto pra usar na apresentação"# freireai-chatbot" 
--- main.py
+++ main.py
@@ -3,12 +3,11 @@ from openai import OpenAI
 import time
 from PIL import Image
 
+from dotenv import load_dotenv
+load_dotenv()
+
 client = OpenAI()
 assistant_id = "asst_gVWsPVMdh1MbgwR2K7Y3Gs9C"
-thread = client.beta.threads.create()
-
-# image = Image.open('mm-logo3.png')
-# st.image(image)
 
 st.title("FreireAI - Eu vou te ajudar a entender o Project Thinking! ")
 
@@ -20,31 +19,35 @@ for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 
+if 'thread_id' not in st.session_state:
+    thread = client.beta.threads.create()
+    st.session_state['thread_id'] = thread.id
+
 if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
 
     client.beta.threads.messages.create(
-        thread_id=thread.id,
+        thread_id=st.session_state['thread_id'],
         role="user",
         content=prompt
     )
 
     run = client.beta.threads.runs.create(
-        thread_id=thread.id,
+        thread_id=st.session_state['thread_id'],
         assistant_id=assistant_id
     )
 
     while run.status != 'completed':
         time.sleep(0.3)
         run = client.beta.threads.runs.retrieve(
-            thread_id=thread.id,
+            thread_id=st.session_state['thread_id'],
             run_id=run.id
         )
 
     # Retrieve messages added by the assistant
     messages = client.beta.threads.messages.list(
-        thread_id=thread.id
+        thread_id=st.session_state['thread_id']
     )
 
     response = messages.to_dict()
